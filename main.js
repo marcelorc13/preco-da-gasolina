838e5576a6bf07bab8349a039d5f11a86b95d8de
Marcelo <marcelorcarnauba@gmail.com>
2023-08-10 00:15:51 -0300

    second-commit

--- main.js
+++ main.js
@@ -2,9 +2,17 @@ const distancia = document.querySelector('#distancia')
 const economia = document.querySelector('#economia')
 const preco = document.querySelector('#preco')
 const botao = document.querySelector('#botaoConverter')
+const resultadoDiv = document.querySelector('#resultadoDiv')
 const resultado = document.querySelector('#resultado')
 
 botao.addEventListener('click', () => {
     let valor = (distancia.value / economia.value) * preco.value
-    resultado.innerHTML = `${valor.toFixed(2)}R$`
+    if (valor > 0 && valor != Infinity) {
+        resultadoDiv.classList.remove('hidden')
+        resultado.innerHTML = `${valor.toFixed(2)}R$`
+    }
+    else {
+        resultadoDiv.classList.remove('hidden')
+        resultado.innerHTML = `Preencha todos os dados`
+    } 
 })
